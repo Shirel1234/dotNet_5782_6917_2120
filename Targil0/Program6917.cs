deddb1f372eac7abb41883fe4d5426b5ccffbbad
Shirel1234 <shirelrn01@gmail.com>
2021-10-12 12:14:32 +0300

    Initial+Welcome function

---
+++ Targil0/Program6917.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6917();
+            Welcome2120();
+            Console.ReadKey();
+        }
+        static partial void Welcome2120();
+        private static void Welcome6917()
+        {
+            Console.WriteLine("Enter your name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", firstName);
+        }
+    }
+}
