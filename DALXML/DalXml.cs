8c84f16adeb9c23d48d1e5fc49cdeaa258ce7568
yiscaGabay <yigabay@g.jct.ac.il>
2022-01-05 20:05:45 +0200

    14:50 5/2

--- DALXML/DalXml.cs
+++ DALXML/DalXml.cs
@@ -58,6 +58,7 @@ namespace Dal
                                             )
                                         )
                                         );
+            
         DalXml() 
         {
             DataSource.Initialize();
